e499f0ffdb51ce3e1e7a7755295be56f58ba4db7
Shreyansh-Banger <106863880+Shreyansh-Banger@users.noreply.github.com>
2023-01-05 19:46:51 +0530

    Update gamepage.js

--- gamepage.js
+++ gamepage.js
@@ -3,14 +3,13 @@ var y = Math.floor(Math.random() * 10 + 1);
 playername= localStorage.getItem("player_name")    
 var guess = 1;
   
-function submit(){
+function sumbit(){
      
 var x = document.getElementById("guessField").value;
 
 if(x == y)
 {    
-   alert("CONGRATULATIONS!!!"+playername+"  YOU GUESSED IT RIGHT IN "
-           + guess + " GUESS ");
+   alert("CONGRATULATIONS!!! "+playername+" YOU GUESSED IT RIGHT IN "+ guess + " GUESS ");
     guess= 1;
 }
 else if(x > y) 
